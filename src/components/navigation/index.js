932c398d8bc91bf08e9d84c3c93c0b039d120de9
jimmabelle <jimmabelle@users.noreply.github.com>
2019-02-11 15:55:26 +0100

    shirts component updated

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { Link } from "react-router-dom"
+// import data from "../../data/data"
+
+import "./style.css"
 
 class Navigation extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="Navigation">
         <Link to="/Shirts">Shirts</Link>
         <Link to="/Accessories">Accessories</Link>
         <Link to="/OurWorld">Our World</Link>
--- src/components/shirts/index.js
+++ src/components/shirts/index.js
@@ -1,12 +1,25 @@
 import React from "react"
 import "./style.css"
 
+import transformation from "../../images/jpg/transformation.jpg"
+
 class Shirts extends React.Component {
 
   render() {
     return (
       <div className="Shirts">
-        Shirts
+        <div>
+          <img className="transformation" src={transformation} alt="transformation mode" />
+        </div>
+        <div>
+          <a className="headline" href="#">Expected the unexpected</a>
+          <p>
+          New year, new you, new style. Time for a change with a collection that offers nothing but.
+          </p>
+        </div>
+        <div>
+          <a className="collection" href="#">Explore Collection</a>
+        </div>
       </div>
     )
   }
